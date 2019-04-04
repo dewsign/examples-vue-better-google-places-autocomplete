27d3c3339f982e49a690d020e4d754f004156b00
Marco Mark <m2de@outlook.com>
2019-04-04 13:34:29 +0100

    Tailwind

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ export default {
 </script>
 
 <style>
+@import 'assets/styles/tailwind.postcss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
